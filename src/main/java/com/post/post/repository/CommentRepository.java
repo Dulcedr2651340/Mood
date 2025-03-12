d46c01d6873c01fc7402a2990c25eb56994a9747
Dulcedr2651340 <dulcedr2651340@hotmail.com>
2025-03-12 18:24:10 -0500

    Capa de servicio COMMENT terminada

--- src/main/java/com/post/post/repository/CommentRepository.java
+++ src/main/java/com/post/post/repository/CommentRepository.java
@@ -3,6 +3,12 @@ package com.post.post.repository;
 import com.post.post.model.entity.Comment;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepository extends JpaRepository<Comment, Integer> {
+
+    List<Comment> findByUserId(Integer userId);
+
+    List<Comment> findByPostId(Integer postId);
 
 }
--- src/main/java/com/post/post/service/ICommentService.java
+++ src/main/java/com/post/post/service/ICommentService.java
@@ -1,4 +1,32 @@
 package com.post.post.service;
 
+import com.post.post.model.entity.Comment;
+import com.post.post.model.entity.dto.CommentRequest;
+import com.post.post.model.entity.dto.CommentResponse;
+
+import java.util.List;
+import java.util.Set;
+
 public interface ICommentService {
+
+    //Listar todos los comentarios
+    List<CommentResponse> getAllComments();
+
+    //Guardar un nuevo comentario
+    CommentResponse saveComment(CommentRequest commentRequest);
+
+    //Actualizar el comentario existente
+    CommentResponse updateComment(Integer id, CommentRequest commentRequest);
+
+    //Buscar un album por su ID
+    CommentResponse findById(Integer id);
+
+    //Obtener Comentarios por un conjunto de Ids
+    List<CommentResponse> getCommentsByUserId(Integer userId);
+
+    //Obtener Comentarios por un conjunto de Ids
+    List<CommentResponse> getCommentsByPostId(Integer postId);
+
+    //Eliminar de manera lógica por su ID
+    void deleteCommentById(Integer id);
 }
