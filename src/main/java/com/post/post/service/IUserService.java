3eebf0a0eba1b7266551e7583d867d7f51b1c4b5
Dulcedr2651340 <dulcedr2651340@hotmail.com>
2025-03-20 19:52:05 -0500

    Definiendo comentarios de la capa servicio de IUSERSERVICE

--- src/main/java/com/post/post/service/IUserService.java
+++ src/main/java/com/post/post/service/IUserService.java
@@ -8,28 +8,28 @@ import java.util.List;
 
 public interface IUserService {
 
-    //
+    // Listar todos los usuarios existentes
     List<UserResponse> getAllUsers();
 
-    //
+    // Guardar un nuevo usuario
     UserResponse saveUser(UserRequest userRequest);
 
-    //
+    // Buscar un usuario por su ID
     UserResponse findById(Integer id);
 
-    //
+    // Actualizar un usuario existente usando su ID
     UserResponse updateId(Integer id, UserRequest userRequest);
 
-    //
+    // Eliminar un usuario de manera lógica usando su ID
     void deleteUserById(Integer id);
 
-    //
+    // Obtener usuarios asociados a un post específico usando el ID del post
     List<UserResponse> getAllUsersByPosts(Integer id);
 
-    //
+    // Obtener usuarios que realizaron comentarios específicos usando el ID del comentario
     List<UserResponse> getAllUserByComments(Integer id);
 
-    //
+    // Obtener usuarios que dieron like a un post específico usando el ID del like
     List<UserResponse> getAllUserBylikes(Integer id);
 
 }
