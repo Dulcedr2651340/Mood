57e164aa1b1b06ae1bb5dcf314bc0845d14deb40
Dulcedr2651340 <dulcedr2651340@hotmail.com>
2025-03-20 19:48:49 -0500

    Definiendo los comentarios de la capa servicio de ITAGSERVICE

--- src/main/java/com/post/post/service/ITagService.java
+++ src/main/java/com/post/post/service/ITagService.java
@@ -8,23 +8,21 @@ import java.util.List;
 
 public interface ITagService {
 
-    //
+    // Listar todos los tags existentes
     List<TagResponse> getAllTags();
 
-    //
+    // Guardar un nuevo tag
     TagResponse saveTag(TagRequest tagRequest);
 
-    //
+    // Actualizar un tag existente usando su ID
     TagResponse updateTag(Integer id, TagRequest tagRequest);
 
-
-    //
+    // Buscar un tag por su ID
     TagResponse findById(Integer id);
 
-
-    //Listar tag por Post
+    // Obtener tags asociados a un post específico usando su ID
     List<TagResponse> getTagsByPostId(Integer postId);
 
-    //Eliminar
+    // Eliminar un tag de manera lógica usando su ID
     void deleteTagById(Integer id);
 }
