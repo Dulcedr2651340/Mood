1d92027ca8db82e8039b54ecfd4309602967784a
Dulcedr2651340 <dulcedr2651340@hotmail.com>
2025-03-26 16:03:46 -0500

    Se implemento el estado a los repository de POST

--- src/main/java/com/post/post/repository/PostRepository.java
+++ src/main/java/com/post/post/repository/PostRepository.java
@@ -1,11 +1,15 @@
 package com.post.post.repository;
 
+import com.post.post.model.entity.Comment;
 import com.post.post.model.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface PostRepository extends JpaRepository<Post, Integer> {
+
+    //Devuelve todos los Post cuyo estado es true(activo)
+    List<Post> findAllByStatusTrue();
 
     List<Post> findByUserId(Integer userId);
     List<Post> findByTagsId(Integer tagId);
