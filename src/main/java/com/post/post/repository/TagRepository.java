0fa48db8d2c43ae4e2823d459b18ddc637704946
Dulcedr2651340 <dulcedr2651340@hotmail.com>
2025-03-26 16:05:05 -0500

    Se implemento el estado a los repository de TAG

--- src/main/java/com/post/post/repository/TagRepository.java
+++ src/main/java/com/post/post/repository/TagRepository.java
@@ -1,11 +1,15 @@
 package com.post.post.repository;
 
+import com.post.post.model.entity.Comment;
 import com.post.post.model.entity.Tag;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
 public interface TagRepository extends JpaRepository<Tag, Integer> {
+
+    //Devuelve todos los Tag cuyo estado es true(activo)
+    List<Tag> findAllByStatusTrue();
 
     // Buscar etiquetas (tags) asociadas a un post específico
     List<Tag> findByPostsId(Integer postId);
